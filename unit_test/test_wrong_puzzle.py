8b8dd82e1f4a42dc41d00f72f982759faa8c43a6
jjauzion <jeremy.jauzion@gmail.com>
2019-02-24 20:13:38 +0100

    wrong test file added

--- unit_test/test_wrong_puzzle.py
+++ unit_test/test_wrong_puzzle.py
@@ -15,3 +15,34 @@ class TestParserWrongPuzzle(unittest.TestCase):
         with self.assertRaises(SystemExit) as cm:
             parser.parser("unit_test/wrong_puzzle/invalid_number.txt")
         self.assertEqual(cm.exception.code, 1)
+
+    def test_nothing(self):
+        with self.assertRaises(SystemExit) as cm:
+            parser.parser("unit_test/wrong_puzzle/nothing.txt")
+        self.assertEqual(cm.exception.code, 1)
+
+    def test_empty(self):
+        with self.assertRaises(SystemExit) as cm:
+            parser.parser("unit_test/wrong_puzzle/empty.txt")
+        self.assertEqual(cm.exception.code, 1)
+
+    def test_lonely_space(self):
+        with self.assertRaises(SystemExit) as cm:
+            parser.parser("unit_test/wrong_puzzle/lonely_space.txt")
+        self.assertEqual(cm.exception.code, 1)
+
+    def test_random(self):
+        with self.assertRaises(SystemExit) as cm:
+            parser.parser("unit_test/wrong_puzzle/random.txt")
+        self.assertEqual(cm.exception.code, 1)
+
+    def test_all(self):
+        test_path = Path("unit_test/wrong_puzzle/")
+        for puzzle in test_path.iterdir():
+            if puzzle.suffix != ".txt":
+                continue
+            with self.assertRaises(SystemExit) as cm:
+                parser.parser(str(puzzle))
+            self.assertEqual(cm.exception.code, 1)
+
+
