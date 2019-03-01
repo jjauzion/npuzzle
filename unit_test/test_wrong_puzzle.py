7b55123d7c13cc5367b2e32d589614be59d802a2
jjauzion <jeremy.jauzion@gmail.com>
2019-03-01 06:59:53 +0100

    unittest wrong updated

--- unit_test/test_wrong_puzzle.py
+++ unit_test/test_wrong_puzzle.py
@@ -46,3 +46,92 @@ class TestParserWrongPuzzle(unittest.TestCase):
             parser.parser("unit_test/wrong_puzzle/comment_in_between.txt")
         self.assertEqual(cm.exception.code, 1)
 
+    def test_doublon(self):
+        with self.assertRaises(SystemExit) as cm:
+            parser.parser("unit_test/wrong_puzzle/doublon.txt")
+        self.assertEqual(cm.exception.code, 1)
+
+    def test_doublon2(self):
+        with self.assertRaises(SystemExit) as cm:
+            parser.parser("unit_test/wrong_puzzle/doublon2.txt")
+        self.assertEqual(cm.exception.code, 1)
+
+    def test_int_max(self):
+        with self.assertRaises(SystemExit) as cm:
+            parser.parser("unit_test/wrong_puzzle/int_max.txt")
+        self.assertEqual(cm.exception.code, 1)
+
+    def test_int_max_2(self):
+        with self.assertRaises(SystemExit) as cm:
+            parser.parser("unit_test/wrong_puzzle/int_max_2.txt")
+        self.assertEqual(cm.exception.code, 1)
+
+    def test_invalid_char(self):
+        with self.assertRaises(SystemExit) as cm:
+            parser.parser("unit_test/wrong_puzzle/invalid_char.txt")
+        self.assertEqual(cm.exception.code, 1)
+
+    def test_invalid_char_2(self):
+        with self.assertRaises(SystemExit) as cm:
+            parser.parser("unit_test/wrong_puzzle/invalid_char_2.txt")
+        self.assertEqual(cm.exception.code, 1)
+
+    def test_invalid_number(self):
+        with self.assertRaises(SystemExit) as cm:
+            parser.parser("unit_test/wrong_puzzle/invalid_number.txt")
+        self.assertEqual(cm.exception.code, 1)
+
+    def test_negative_space(self):
+        with self.assertRaises(SystemExit) as cm:
+            parser.parser("unit_test/wrong_puzzle/negative_space.txt")
+        self.assertEqual(cm.exception.code, 1)
+
+    def test_too_many_columns(self):
+        with self.assertRaises(SystemExit) as cm:
+            parser.parser("unit_test/wrong_puzzle/too_many_columns.txt")
+        self.assertEqual(cm.exception.code, 1)
+
+    def test_too_many_columns_2(self):
+        with self.assertRaises(SystemExit) as cm:
+            parser.parser("unit_test/wrong_puzzle/too_many_columns_2.txt")
+        self.assertEqual(cm.exception.code, 1)
+
+    def test_too_many_lines(self):
+        with self.assertRaises(SystemExit) as cm:
+            parser.parser("unit_test/wrong_puzzle/too_many_lines.txt")
+        self.assertEqual(cm.exception.code, 1)
+
+    def test_wrong_1st_line(self):
+        with self.assertRaises(SystemExit) as cm:
+            parser.parser("unit_test/wrong_puzzle/wrong_1st_line.txt")
+        self.assertEqual(cm.exception.code, 1)
+
+    def test_wrong_1st_line_2(self):
+        with self.assertRaises(SystemExit) as cm:
+            parser.parser("unit_test/wrong_puzzle/wrong_1st_line_2.txt")
+        self.assertEqual(cm.exception.code, 1)
+
+    def test_wrong_1st_line_3(self):
+        with self.assertRaises(SystemExit) as cm:
+            parser.parser("unit_test/wrong_puzzle/wrong_1st_line_3.txt")
+        self.assertEqual(cm.exception.code, 1)
+
+    def test_wrong_size(self):
+        with self.assertRaises(SystemExit) as cm:
+            parser.parser("unit_test/wrong_puzzle/wrong_size.txt")
+        self.assertEqual(cm.exception.code, 1)
+
+    def test_wrong_size_2(self):
+        with self.assertRaises(SystemExit) as cm:
+            parser.parser("unit_test/wrong_puzzle/wrong_size_2.txt")
+        self.assertEqual(cm.exception.code, 1)
+
+    def test_wrong_size_3(self):
+        with self.assertRaises(SystemExit) as cm:
+            parser.parser("unit_test/wrong_puzzle/wrong_size_3.txt")
+        self.assertEqual(cm.exception.code, 1)
+
+    def test_too_many_columns_2(self):
+        with self.assertRaises(SystemExit) as cm:
+            parser.parser("unit_test/wrong_puzzle/too_many_columns_2.txt")
+        self.assertEqual(cm.exception.code, 1)
