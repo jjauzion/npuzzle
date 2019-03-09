5fa9bb8e719c02087db0bf4fe486e0aa70c8d100
jjauzion <jjauzion@student.42.fr>
2019-03-09 15:20:22 +0100

    ongoing

--- unit_test/test_valid_puzzle.py
+++ unit_test/test_valid_puzzle.py
@@ -6,6 +6,7 @@ from pathlib import Path
 from src import PathFinder
 from src import Node
 from src import config
+from . import n-puzzle
 
 
 class TestValidPuzzle(unittest.TestCase):
@@ -23,6 +24,15 @@ class TestValidPuzzle(unittest.TestCase):
         self.assertEqual(self.path.solution["nb_move"], solution["nb_move"])
 
     def test_puzzle_2(self):
+        config.TAQUIN_SIZE = 3
+        start_node = Node.Node(grid=[4, 7, 6, 3, 0, 5, 1, 8, 2])
+        with Path("unit_test/solution/{}.pkl".format(start_node.id)).open(mode='rb') as file:
+            solution = pickle.load(file)
+        self.path.set_start_node(start_node)
+        self.path.a_star()
+        self.assertEqual(self.path.solution["nb_move"], solution["nb_move"])
+
+    def test_puzzle_3(self):
         config.TAQUIN_SIZE = 3
         start_node = Node.Node(grid=[4, 7, 6, 3, 0, 5, 1, 8, 2])
         with Path("unit_test/solution/{}.pkl".format(start_node.id)).open(mode='rb') as file:
